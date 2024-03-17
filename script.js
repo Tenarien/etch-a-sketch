46d05203328a096f0ac7afb03fb4daf9d41d7143
Krzysztof Romanowski <mamateki7@gmail.com>
2024-03-17 19:04:10 +0000

    Add function to add squares based on the inputAmount

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const content = document.querySelector(".content");
 
 
-const square = document.createElement("div");
-square.classList.add("square");
 
 function addSquare(inputAmount) {
     for (let i = 0; i <= inputAmount; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
         content.appendChild(square);
     }
 }
@@ -15,11 +15,9 @@ const amountBtn = document.querySelector(".amountBtn");
 amountBtn.addEventListener("click", () => {
     let inputAmount = prompt("Please input the amount of squares: ")
     inputAmount = parseInt(inputAmount);
-    console.log(inputAmount)
     if (inputAmount < 0 || inputAmount > 65 || typeof inputAmount !== "number") {
-        console.log(inputAmount)
         alert("You provided a wrong number (Max 64)");
     } else {
-        return inputAmount;
+        addSquare(inputAmount);
     }
 });
