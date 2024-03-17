2f604cead893337e42c1be2df5e9ff630d168da4
Krzysztof Romanowski <mamateki7@gmail.com>
2024-03-17 18:50:00 +0000

    Add button listener to create a prompt to enter number of square

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const content = document.querySelector(".content");
 const square = document.createElement("div");
 square.classList.add("square");
 content.appendChild(square);
+
+
+
+const amountBtn = document.querySelector(".amountBtn");
+amountBtn.addEventListener("click", () => {
+    let inputAmount = prompt("Please input the amount of squares: ")
+    if (inputAmount < 0 || typeof inputAmount !== "number") {
+        alert("You provided a wrong number (Max 64)");
+    } else {
+        return inputAmount;
+    }
+});
