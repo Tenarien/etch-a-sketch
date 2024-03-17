8f7a01d2a65fd43113ae114e48fda5de20890d2f
Krzysztof Romanowski <mamateki7@gmail.com>
2024-03-17 19:01:32 +0000

    Fix logic of inputAmount prompt

--- script.js
+++ script.js
@@ -3,14 +3,21 @@ const content = document.querySelector(".content");
 
 const square = document.createElement("div");
 square.classList.add("square");
-content.appendChild(square);
 
+function addSquare(inputAmount) {
+    for (let i = 0; i <= inputAmount; i++) {
+        content.appendChild(square);
+    }
+}
 
 
 const amountBtn = document.querySelector(".amountBtn");
 amountBtn.addEventListener("click", () => {
     let inputAmount = prompt("Please input the amount of squares: ")
-    if (inputAmount < 0 || typeof inputAmount !== "number") {
+    inputAmount = parseInt(inputAmount);
+    console.log(inputAmount)
+    if (inputAmount < 0 || inputAmount > 65 || typeof inputAmount !== "number") {
+        console.log(inputAmount)
         alert("You provided a wrong number (Max 64)");
     } else {
         return inputAmount;
