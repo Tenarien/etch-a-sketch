bd7e96f7dea839bd2c11eeab93dd2d991328c72f
Krzysztof Romanowski <mamateki7@gmail.com>
2024-03-18 16:38:27 +0000

    Add function to add event listener to each square in the node list and change background color when mouse is over the square

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 const content = document.querySelector(".content");
 
+
 const defaultAmount = 16;
 
-for (let i = 0; i <= defaultAmount; i++) {
+for (let i = 0; i < defaultAmount; i++) {
     const squareX = document.createElement("div");
     squareX.classList.add("squareX");
     content.appendChild(squareX);
-    for (let i = 0; i <= defaultAmount; i++) {
+    for (let i = 0; i < defaultAmount; i++) {
         const squareY = document.createElement("div");
         squareY.classList.add("squareY");
         squareY.style.cssText = `height: calc(800px / ${defaultAmount}); width: calc(800px / ${defaultAmount});`;
+        squareY.setAttribute.onmouseenter = "color()";
         squareX.appendChild(squareY);
     }
 }
+
+
+
+function addColor() {
+    const squareColor = document.querySelectorAll(".squareY");
+    console.log(squareColor);
+    squareColor.forEach(square => {
+        square.addEventListener('mouseover', () => {
+            square.style.backgroundColor = "black";
+        });
+    }); 
+}
+
 
 
 function addSquare(inputAmount) {
@@ -20,17 +35,19 @@ function addSquare(inputAmount) {
         content.removeChild(content.firstChild);
     }
 
-    for (let i = 0; i <= inputAmount; i++) {
+    for (let i = 0; i < inputAmount; i++) {
         const squareX = document.createElement("div");
         squareX.classList.add("squareX");
         content.appendChild(squareX);
-        for (let i = 0; i <= inputAmount; i++) {
+        for (let i = 0; i < inputAmount; i++) {
             const squareY = document.createElement("div");
             squareY.classList.add("squareY");
             squareY.style.cssText = `height: calc(800px / ${inputAmount}); width: calc(800px / ${inputAmount});`;
             squareX.appendChild(squareY);
+            
         }
     }
+    addColor()
     
 }
 
@@ -45,3 +62,4 @@ amountBtn.addEventListener("click", () => {
         addSquare(inputAmount);
     }
 });
+
